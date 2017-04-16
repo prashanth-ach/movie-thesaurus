eee762f816cd4123a7bb29433a9f3ba20847125b
prashanth-ach <prashanth.ach@gmail.com>
2017-04-16 18:21:48 -0500

    added cast and crew scraper

--- src/main/java/org/learning/imdb/scrape/CastAndCrewScraper.java
+++ src/main/java/org/learning/imdb/scrape/CastAndCrewScraper.java
@@ -1,5 +1,14 @@
 package org.learning.imdb.scrape;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.learning.imdb.DataObjects.CastAndCrew;
 
 public class CastAndCrewScraper extends AbstractScraper {
@@ -16,6 +25,36 @@ public class CastAndCrewScraper extends AbstractScraper {
 
 	@Override
 	public void extractInfo() {
+		Map<String, ArrayList<String>> t = new HashMap<String, ArrayList<String>>();
+		Elements credit = doc.getElementsByTag("tr");
+
+		for (Element element : credit) {
+			String key = "";
+			try {
+				key = element.getElementsByClass("name").first().text();
+			} catch (Exception e) {
+				key = "";
+			}
+			try {
+				Elements link = element.select("td.name > a");
+				String url = link.attr("href");
+				String s = "http://www.imdb.com" + url;
+				String p = key + "|" + element.getElementsByClass("credit").first().text();
+				if (t.containsKey(s)) {
+					t.get(s).add(p);
+				} else {
+					ArrayList<String> temp = new ArrayList<String>();
+					temp.add(p);
+					t.put(s, temp);
+
+				}
+
+			} catch (Exception e) {
+
+			}
+
+		}
+		System.out.println(t);
 
 	}
 
